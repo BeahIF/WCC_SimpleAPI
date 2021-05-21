fddc74a60befea869b4b44e54fccb029aafd0f6f
BeahIF <beatrizdacosta1@gmail.com>
2021-05-20 22:10:14 -0300

    Enviando aula 4

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,13 @@
+const Agendamento = require('../models/Agendamento')
 module.exports = app => {
-    app.get('/',(req,resp)=>{
-        resp.send("Servidor OK!")
+    app.get('/agendamentos',(req,resp)=>{
+        // resp.send("Servidor OK!")
+        Agendamento.listagem(resp);
     });
+    app.post('/agendamentos',(req,resp)=>{
+        const agendamento = req.body;
+        // console.log(agendamento)
+        Agendamento.inserir(agendamento,resp);
+        // resp.send(agendamento)
+    })
 };
